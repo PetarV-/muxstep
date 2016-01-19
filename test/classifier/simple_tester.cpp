3c205729e43cc1b2acd7df0d42c902d2ce212fca
Petar Veličković <pv273@cam.ac.uk>
2016-01-19 17:18:57 +0000

    Modifications to the simple tester interface

--- test/classifier/simple_tester.cpp
+++ test/classifier/simple_tester.cpp
@@ -33,20 +33,29 @@ typedef unsigned long long llu;
 
 int main(int argc, char **argv)
 {
-    if (argc != 5)
+    if (argc != 3 && argc != 5)
     {
-        printf("Usage: ./simple_tester <node_count> <data_set_file> <noise_mean_lo>:<noise_mean_step>:<noise_mean_hi> <noise_stddev_lo>:<noise_stddev_step>:<noise_stddev_hi>\n");
+        printf("Usage: ./simple_tester <node_count> <data_set_file> (<noise_mean_lo>:<noise_mean_step>:<noise_mean_hi> <noise_stddev_lo>:<noise_stddev_step>:<noise_stddev_hi>)\n");
         return -1;
     }
     
     int node_count;
     sscanf(argv[1], "%d", &node_count);
     tuple<int, int, vector<pair<vector<pair<int, vector<double> > >, bool> > > data = extract_data(argv[2]);
+    
     double mu_lo, mu_step, mu_hi;
     double sigma_lo, sigma_step, sigma_hi;
-    sscanf(argv[3], "%lf:%lf:%lf", &mu_lo, &mu_step, &mu_hi);
-    sscanf(argv[4], "%lf:%lf:%lf", &sigma_lo, &sigma_step, &sigma_hi);
-    
+    if (argc == 5) 
+    {
+        sscanf(argv[3], "%lf:%lf:%lf", &mu_lo, &mu_step, &mu_hi);
+        sscanf(argv[4], "%lf:%lf:%lf", &sigma_lo, &sigma_step, &sigma_hi);
+    }
+    else
+    {
+        mu_lo = mu_hi = sigma_lo = sigma_hi = 0.0;
+        mu_step = sigma_step = 1.0;
+    }
+
     Classifier<vector<pair<int, vector<double> > >, bool> *C = new MultiplexGMHMMClassifier(node_count, get<0>(data), get<1>(data));
 
     noise_test(C, get<2>(data), mu_lo, mu_step, mu_hi, sigma_lo, sigma_step, sigma_hi);
