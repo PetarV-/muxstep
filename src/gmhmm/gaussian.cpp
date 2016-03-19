4103b9b2f4ee018d0c67614bfab955b123b21f3a
Petar Veličković <pv273@cam.ac.uk>
2016-03-19 03:57:31 +0000

    Commented the Gaussian distribution implementation.

--- src/gmhmm/gaussian.cpp
+++ src/gmhmm/gaussian.cpp
@@ -26,22 +26,25 @@ using namespace std;
 #define DPRINTLLD(LLD) printf(#LLD " = %lld\n", (LLD))
 #define DPRINTLF(LF) printf(#LF " = %.5lf\n", (LF))
 
+// helper function; Phi(x; mean, stddev)
 double Gaussian::gaussian_pdf(double x, double mean, double stdev)
 {
+    // simply compute exp(-(x - mu)^2/2*sigma^2)/sqrt(2*pi*sigma^2)
     double E = x - mean;
     E *= -E;
     E /= 2 * stdev * stdev;
     double ret = exp(E);
     return ret / (stdev * sqrt(2 * M_PI));
 }
-    
 
+// create a new untrained Gaussian distribution with a known number of sub-outputs
 Gaussian::Gaussian(int sub_count) : obs(sub_count)
 {
     mu = new double[sub_count];
     sigma = new double[sub_count];
 }
 
+// copy a known Gaussian distribution from its parameters
 Gaussian::Gaussian(int sub_count, double *mu, double *sigma) : obs(sub_count)
 {
     this -> mu = new double[obs];
@@ -59,23 +62,25 @@ Gaussian::~Gaussian()
     delete[] sigma;
 }
 
+// Create a (deep) copy of the distribution
 Distribution* Gaussian::clone()
 {
     return new Gaussian(obs, mu, sigma);
 }
-    
+
+// Train the distribution's parameters on a training set
 void Gaussian::train(vector<vector<pair<int, double> > > &train_set)
 {
     int *cnt = new int[obs];
-        
-    // get sample means and std. deviations
+    
     for (int i=0;i<obs;i++)
     {
         mu[i] = 0.0;
         sigma[i] = 0.0;
         cnt[i] = 0;
     }
-        
+    
+    // compute sample means and std. deviations for each sub-output individually
     for (uint i=0;i<train_set.size();i++)
     {
         for (uint j=0;j<train_set[i].size();j++)
@@ -99,12 +104,15 @@ void Gaussian::train(vector<vector<pair<int, double> > > &train_set)
         
     delete cnt;
 }
-    
+
+// Get the probability of producing output x, assuming the sub-output is obs_id
 double Gaussian::get_probability(int obs_id, double x)
 {
+    // simply return the already defined probability density function
     return gaussian_pdf(x, mu[obs_id], sigma[obs_id]);
 }
-    
+
+// read the distribution from an input stream
 istream& Gaussian::read(istream &in)
 {
     in >> this -> obs;
@@ -117,7 +125,8 @@ istream& Gaussian::read(istream &in)
     
     return in;
 }
-    
+
+// write the distribution to an output stream
 ostream& Gaussian::write(ostream &out)
 {
     out << obs << endl;
